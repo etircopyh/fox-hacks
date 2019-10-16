e339dd15137c66b3db7dc95f48c8d1108e3c5c1c
Ivan <yumteej@gmail.com>
2019-10-16 21:25:30 +0300

    symlink sanitized search engines

--- symlink-ff-profile.sh
+++ symlink-ff-profile.sh
@@ -3,3 +3,4 @@ ffprofile=$(ls ~/.mozilla/firefox/ | grep default-release)
 
 ln -rsfv ./firefox-user.js ~/.mozilla/firefox/$ffprofile/user.js
 ln -rsfv ./chrome/ ~/.mozilla/firefox/$ffprofile/
+ln -rsfv ./search.json.mozlz4 ~/.mozilla/firefox/$ffprofile/
