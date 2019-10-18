188c95eee31198d0615a94c9b6e53470cad748e1
Ivan <yumteej@gmail.com>
2019-10-19 01:33:49 +0300

    rename script

--- update-config.sh
+++ update-config.sh
@@ -4,3 +4,6 @@ ffprofile=$(ls ~/.mozilla/firefox/ | grep default-release)
 ln -rsfv ./firefox-user.js ~/.mozilla/firefox/$ffprofile/user.js
 ln -rsfv ./chrome/ ~/.mozilla/firefox/$ffprofile/
 ln -rsfv ./search.json.mozlz4 ~/.mozilla/firefox/$ffprofile/
+
+# Update adblock css rules
+curl -s http://www.floppymoose.com/userContent.css > ./chrome/css/adblock.css
