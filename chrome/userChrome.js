af50a8e746c5272ff92e2257915ac78919434363
Ivan <etircopyhdot@gmail.com>
2020-02-23 00:25:53 +0200

    format js

--- chrome/userChrome.js
+++ chrome/userChrome.js
@@ -1,26 +1,25 @@
 (function() {
     // IMPORTANT: when there's no filename, be sure to include a trailing slash at the end.
-    const mypage = "file:///home/yumtee/GitHubRepos/hypostartpage/index.html";
+    const mypage = 'file:///home/yumtee/GitRepos/hypostartpage/index.html';
     // Don't place the caret in the location bar. Useful if you want a page's search box to have focus instead.
-    var removefocus = "no";
+    var removefocus = 'no';
     // Clear the page's URL from the location bar. Normally not needed, as this should already be the default behavior.
-    var clearlocationbar = "no";
-    
+    var clearlocationbar = 'no';
+
     aboutNewTabService.newTabURL = mypage;
-    function customNewTab () {
-        if (removefocus == "yes") {
+    function customNewTab() {
+        if (removefocus == 'yes') {
             setTimeout(function() {
                 gBrowser.selectedBrowser.focus();
             }, 0);
         }
-        if (clearlocationbar == "yes") {
+        if (clearlocationbar == 'yes') {
             setTimeout(function() {
                 if (gBrowser.selectedBrowser.currentURI.spec == mypage) {
-                    window.document.getElementById("urlbar").value = "";
+                    window.document.getElementById('urlbar').value = '';
                 }
             }, 1000);
         }
     }
-    gBrowser.tabContainer.addEventListener("TabOpen", customNewTab, false);
-    
-}());
+    gBrowser.tabContainer.addEventListener('TabOpen', customNewTab, false);
+})();
