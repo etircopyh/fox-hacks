c151cf43a44ef440a5e9806c70240634222d7fa6
Ivan <etircopyhdot@gmail.com>
2020-04-30 04:45:12 +0300

    script fix

--- update-config.sh
+++ update-config.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ffprofile=$(ls ~/.mozilla/firefox/ | grep default-release)
 # default-release = Firefox
 # default-beta = Firefox Beta
@@ -7,7 +7,7 @@ ffprofile=$(ls ~/.mozilla/firefox/ | grep default-release)
 echo 'Symlinking Firefox profile'
 ln -rsfv ./firefox-user.js ~/.mozilla/firefox/$ffprofile/user.js
 ln -rsfv ./chrome/ ~/.mozilla/firefox/$ffprofile/
-ln -rsfv ./search.json.mozlz4 ~/.mozilla/firefox/$ffprofile/
+#ln -rsfv ./search.json.mozlz4 ~/.mozilla/firefox/$ffprofile/
 
 # Update adblock css rules
 curl -s http://www.floppymoose.com/userContent.css > ./chrome/import/userContent/adblock.css && echo 'adblock.css updated'
